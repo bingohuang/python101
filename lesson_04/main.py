543dc3a4584d2a47f4caebea4a46f02723b4c956
bingohuang <770581715@qq.com>
2024-01-18 18:29:56 +0800

    add lesson 04

---
+++ lesson_04/main.py
@@ -0,0 +1,20 @@
+msg = 'This is \'bingo\'.'
+
+desc = 'This' 'is' \
+       'bingo'
+
+# r'' raw
+# f'' format
+
+print(msg)
+print(desc)
+
+msg = 'bingo'
+print(len(msg))
+print(msg[-1])
+print(msg[0])
+print(msg[1])
+print(msg[0:])
+print(msg[3:])
+print(msg[:5])
+print(msg[:6])
